ff9027de2c58dfe3ebee739f8434672ac65464f1
Djuri Schiffer <djuri_schiffer@hotmail.com>
2022-09-10 21:55:07 +0200

    Fixed typos

--- src/components/TableHead.jsx
+++ src/components/TableHead.jsx
@@ -18,10 +18,10 @@ const TableHead = ({ className, type }) => {
         <tr>
           <th className="text-left py-2 pl-3">Amount</th>
           <th className="text-left py-2">Purchase price</th>
-          <th className="text-left py-2">purchaseDate</th>
-          <th className="text-left py-2">currentValue</th>
-          <th className="text-left py-2">averagePurchasePrice</th>
-          <th className="text-left py-2">averagePurchaseDifference</th>
+          <th className="text-left py-2">Purchase Date</th>
+          <th className="text-left py-2">Current Value</th>
+          <th className="text-left py-2">Average Purchase Price</th>
+          <th className="text-left py-2">Average Purchase Difference</th>
           <th className="text-right py-2 pr-3">Actions</th>
         </tr>
       </thead>
