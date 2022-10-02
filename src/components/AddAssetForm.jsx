3a2973ddc6ba1518a3050602f0fdafc5d8a9b04f
Djuri Schiffer <djuri_schiffer@hotmail.com>
2022-10-02 13:51:45 +0200

    moved assetform to own component

--- src/components/AddAssetForm.jsx
+++ src/components/AddAssetForm.jsx
@@ -1,7 +1,55 @@
-const AddAssetForm = ({ onSubmit, children, className }) => {
+const AddAssetForm = ({
+  onSubmit,
+  className,
+  amount,
+  price,
+  date,
+  handleChange,
+  formType
+}) => {
   return (
     <form onSubmit={onSubmit} className={className}>
-      {children}
+      <label for="amount" className="text-gray text-sm mb-1">
+        Amount
+      </label>
+      <input
+        className="text-black mb-3 p-2 shadow-line rounded"
+        name="amount"
+        type="number"
+        placeholder="amount"
+        onChange={handleChange}
+        value={amount}
+        required
+      />
+      <label for="purchasePrice" className="text-gray text-sm mb-1">
+        Purchase Price - in Euros
+      </label>
+      <input
+        className="text-black mb-3 p-2 shadow-line rounded"
+        name="purchasePrice"
+        type="number"
+        placeholder="purchase price"
+        onChange={handleChange}
+        value={price}
+        required
+      />
+      <label for="date" className="text-gray text-sm mb-1">
+        Purchase Date
+      </label>
+      <input
+        className="text-black mb-4 p-2 shadow-line rounded"
+        name="date"
+        type="date"
+        placeholder="date"
+        onChange={handleChange}
+        value={date}
+        required
+      />
+      <input
+        className="bg-green p-4 rounded-md"
+        type="submit"
+        value={formType === "add" ? "Add asset" : "Edit asset"}
+      />
     </form>
   );
 };
--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -340,49 +340,15 @@ const Overview = () => {
         open={openAddAssetModal}
         title={formType === "add" ? "Add asset" : "Edit asset"}
       >
-        <AddAssetForm onSubmit={handleSubmit} className={"flex flex-col"}>
-          <label for="amount" className="text-gray text-sm mb-1">
-            Amount
-          </label>
-          <input
-            className="text-black mb-3 p-2 shadow-line rounded"
-            name="amount"
-            type="number"
-            placeholder="amount"
-            onChange={handleChange}
-            value={amount}
-            required
-          />
-          <label for="purchasePrice" className="text-gray text-sm mb-1">
-            Purchase Price - in Euros
-          </label>
-          <input
-            className="text-black mb-3 p-2 shadow-line rounded"
-            name="purchasePrice"
-            type="number"
-            placeholder="purchase price"
-            onChange={handleChange}
-            value={price}
-            required
-          />
-          <label for="date" className="text-gray text-sm mb-1">
-            Purchase Date
-          </label>
-          <input
-            className="text-black mb-4 p-2 shadow-line rounded"
-            name="date"
-            type="date"
-            placeholder="date"
-            onChange={handleChange}
-            value={date}
-            required
-          />
-          <input
-            className="bg-green p-4 rounded-md"
-            type="submit"
-            value={formType === "add" ? "Add asset" : "Edit asset"}
-          />
-        </AddAssetForm>
+        <AddAssetForm
+          onSubmit={handleSubmit}
+          className={"flex flex-col"}
+          amount={amount}
+          price={price}
+          date={date}
+          handleChange={handleChange}
+          formType={formType}
+        />
       </Modal>
       <Modal
         onClose={() => setOpenRemoveAssetModal(false)}
