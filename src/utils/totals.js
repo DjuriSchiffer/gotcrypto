b89c268ec0f13315ee6de6d496fbcfce82267b08
Djuri Schiffer <djuri_schiffer@hotmail.com>
2022-10-21 17:22:54 +0200

    take floating numbers in totals

--- src/utils/totals.js
+++ src/utils/totals.js
@@ -1,32 +1,60 @@
-import { PercentageDifference, PercentageFormat, CurrencyFormat, AveragePurchasePrice, Profit, CurrentValue, DateFormat } from '../utils/CalculateHelpers';
+import {
+  PercentageDifference,
+  AveragePurchasePrice,
+  CurrentValue,
+} from "../utils/CalculateHelpers";
 
 const totals = (assets = [], currentCurrency = {}) => {
-    const totalAmount = assets.reduce((acc, asset) => acc + parseInt(asset.amount), 0);
-    const totalPurchasePrice = assets.reduce((acc, asset) => acc + parseInt(asset.purchasePrice), 0);
-    const totalValue = CurrentValue(totalAmount, currentCurrency.price);
-    const totalAveragePurchasePrice = AveragePurchasePrice(totalPurchasePrice, totalAmount);
-    const totalPercentageDifference = PercentageDifference(totalPurchasePrice, totalValue);
+  const totalAmount = assets.reduce(
+    (acc, asset) => acc + parseFloat(asset.amount),
+    0
+  );
+  const totalPurchasePrice = assets.reduce(
+    (acc, asset) => acc + parseFloat(asset.purchasePrice),
+    0
+  );
+  const totalValue = CurrentValue(totalAmount, currentCurrency.price);
+  console.log("totalValue", totalValue);
+  const totalAveragePurchasePrice = AveragePurchasePrice(
+    totalPurchasePrice,
+    totalAmount
+  );
+  const totalPercentageDifference = PercentageDifference(
+    totalPurchasePrice,
+    totalValue
+  );
 
-    return {
-        totalAmount: assets.length > 0 ? totalAmount : 0,
-        totalPurchasePrice: assets.length > 0 ? totalPurchasePrice : 0,
-        totalValue : assets.length > 0 ? totalValue : 0,
-        totalAveragePurchasePrice: assets.length > 0 ? totalAveragePurchasePrice : 0,
-        totalPercentageDifference: assets.length > 0 ? totalPercentageDifference : 0
-    }
-}
+  return {
+    totalAmount: assets.length > 0 ? totalAmount : 0,
+    totalPurchasePrice: assets.length > 0 ? totalPurchasePrice : 0,
+    totalValue: assets.length > 0 ? totalValue : 0,
+    totalAveragePurchasePrice:
+      assets.length > 0 ? totalAveragePurchasePrice : 0,
+    totalPercentageDifference:
+      assets.length > 0 ? totalPercentageDifference : 0,
+  };
+};
 
 export const getGlobalTotals = (assets = []) => {
-    const totalValue = assets.reduce((acc, asset) => acc + parseInt(asset.totals.totalValue), 0);
-    const totalPurchasePrice = assets.reduce((acc, asset) => acc + parseInt(asset.totals.totalPurchasePrice), 0);
-    const totalPercentageDifference = PercentageDifference(totalPurchasePrice, totalValue);
+  const totalValue = assets.reduce(
+    (acc, asset) => acc + parseFloat(asset.totals.totalValue),
+    0
+  );
+  const totalPurchasePrice = assets.reduce(
+    (acc, asset) => acc + parseFloat(asset.totals.totalPurchasePrice),
+    0
+  );
+  const totalPercentageDifference = PercentageDifference(
+    totalPurchasePrice,
+    totalValue
+  );
 
-    return {
-        totalValue : assets.length > 0 ? totalValue : 0,
-        totalPurchasePrice: assets.length > 0 ? totalPurchasePrice : 0,
-        totalPercentageDifference: assets.length > 0 ? totalPercentageDifference : 0
-    }
-
-}
+  return {
+    totalValue: assets.length > 0 ? totalValue : 0,
+    totalPurchasePrice: assets.length > 0 ? totalPurchasePrice : 0,
+    totalPercentageDifference:
+      assets.length > 0 ? totalPercentageDifference : 0,
+  };
+};
 
 export default totals;
