e66dc6eb03a2c9130434a9bda32da8f7f2d47ceb
Djuri Schiffer <djuri_schiffer@hotmail.com>
2023-01-30 21:19:24 +0100

    Moved Navbar to own component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Button, Navbar } from "flowbite-react";
+import logo from "../public/images/logo.svg";
+import { Link } from "react-router-dom";
+import Icon from "./Icon";
+
+const Nav = () => {
+  return (
+    <Navbar fluid={true} rounded={true}>
+      <Link
+        to={"/"}
+        className={"mr-auto flex flex-wrap items-center justify-between"}
+      >
+        <img src={logo} className="mr-3 h-6 sm:h-9" alt="Got Crypto" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Got Crypto
+        </span>
+      </Link>
+      <div className="flex md:order-2">
+        <Button
+          href="https://github.com/DjuriSchiffer/finance-portfolio"
+          color={"gray"}
+          target={"_blank"}
+          rel={"noreferer"}
+        >
+          <Icon id="Github" />
+        </Button>
+      </div>
+    </Navbar>
+  );
+};
+
+export default Nav;
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,33 +1,9 @@
-import { Button, Navbar } from "flowbite-react";
-import logo from "../public/images/logo.svg";
-import { Link } from "react-router-dom";
-import Icon from "./Icon";
+import Navbar from "./Navbar";
 
 const Page = ({ children }) => {
   return (
     <main className="bg-white dark:bg-gray-dark min-h-screen">
-      <Navbar fluid={true} rounded={true}>
-        <Link
-          to={"/"}
-          className={"mr-auto flex flex-wrap items-center justify-between"}
-        >
-          <img src={logo} className="mr-3 h-6 sm:h-9" alt="Got Crypto" />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Got Crypto
-          </span>
-        </Link>
-
-        <div className="flex md:order-2">
-          <Button
-            href="https://github.com/DjuriSchiffer/finance-portfolio"
-            color={"gray"}
-            target={"_blank"}
-            rel={"noreferer"}
-          >
-            <Icon id="Github" />
-          </Button>
-        </div>
-      </Navbar>
+      <Navbar />
       <div className={"p-4"}>{children}</div>
     </main>
   );
