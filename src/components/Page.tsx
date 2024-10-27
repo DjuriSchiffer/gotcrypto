f8ea7624ab17a1904ddfa0c58ca565aa889cb8ce
Djuri Schiffer <djurischiffer@gmail.com>
2024-10-27 20:53:55 +0100

    add some styling

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -15,7 +15,7 @@ const Page: React.FC<PageProps> = ({ children }) => {
   return (
     <main className="bg-white dark:bg-gray-dark min-h-screen">
       <div className="p-4">
-        <div className="grid gap-4 mb-4 w-full grid-cols-6">
+        <div className="grid gap-4 w-full grid-cols-6">
           <div className='col-span-1'>
             <SideBar />
           </div>
