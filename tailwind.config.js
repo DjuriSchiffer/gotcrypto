9d753e665b79eaaa78354feb2e901544838fcbc7
dschiffer <dschiffer@anwb.nl>
2024-11-03 20:49:20 +0100

    restored extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,29 @@ const config = {
       "gray-dark": "#1e2126",
       gray: "#8492a6",
     },
+    extend: {
+      colors: {
+        blue: {
+          // primary
+          50: "#ecfdf5",
+          100: "#d1fae5",
+          200: "#a7f3d0",
+          300: "#6ee7b7",
+          400: "#34d399",
+          500: "#10b981",
+          600: "#10b981",
+          700: "#10b981",
+          800: "#10b981",
+          900: "#10b981",
+          DEFAULT: "#34d399", //500
+          on: "#ecfdf5", //50
+          dark: {
+            DEFAULT: "#34d399", //400
+            on: "#ecfdf5", //800
+          },
+        },
+      }
+    }
   },
   plugins: [require("flowbite/plugin")],
   darkMode: "class",
