ae60d050fc03bf1aeea075bf5e646d1a6c2dda27
Djuri Schiffer <dschiffer@anwb.nl>
2024-11-15 13:47:15 +0100

    Added styling to ChangeQuote

--- src/components/ChangeQuote.tsx
+++ src/components/ChangeQuote.tsx
@@ -3,32 +3,50 @@ import { FaDollarSign, FaEuroSign } from "react-icons/fa";
 import { CurrencyQuote } from "api";
 import { useQueryClient } from '@tanstack/react-query';
 import { useStorage } from "../hooks/useStorage";
+import { useMemo } from "react";
+import classNames from "classnames";
+
+interface PriceOption {
+    quote: keyof CurrencyQuote;
+    symbol: React.ReactNode;
+}
 
 export function ChangeQuote() {
     const { setCurrencyQuote, currencyQuote } = useStorage();
     const queryClient = useQueryClient();
 
-    const handleCurrencyQuote = (quote: keyof CurrencyQuote) => {
-        setCurrencyQuote(quote)
+    const handleQuoteChange = (quote: keyof CurrencyQuote) => {
+        setCurrencyQuote(quote);
         queryClient.invalidateQueries({ queryKey: ['fetchedCurrencies'] });
     };
 
+    const priceOptions: PriceOption[] = useMemo(() => [
+        {
+            quote: 'EUR',
+            symbol: <FaEuroSign className="mr-1" />,
+        },
+        {
+            quote: 'USD',
+            symbol: <FaDollarSign className="mr-1" />,
+        },
+    ], []);
+
     return (
         <Button.Group>
-            <Button
-                color={currencyQuote === 'EUR' ? 'dark' : 'gray'}
-                onClick={() => handleCurrencyQuote('EUR')}
-            >
-                <FaEuroSign className="mr-1" />
-                Euro
-            </Button>
-            <Button
-                color={currencyQuote === 'USD' ? 'dark' : 'gray'}
-                onClick={() => handleCurrencyQuote('USD')}
-            >
-                <FaDollarSign className="mr-1" />
-                Dollar
-            </Button>
+            {priceOptions.map((option, index) => (
+                <Button
+                    color={currencyQuote === option.quote ? 'dark' : 'gray'}
+                    onClick={() => handleQuoteChange(option.quote)}
+                    className={classNames('', {
+                        '!border-blue-400': currencyQuote === option.quote,
+                        'border-l': index > 0 && currencyQuote === option.quote,
+                        'border-l-[1px]': index > 0 && currencyQuote === option.quote,
+                    })}
+                >
+                    {option.symbol}
+                    {option.quote}
+                </Button>
+            ))}
         </Button.Group>
     );
 }
