1d37555ffb7371c348f0a7eeb9c53a0fcf3a26c3
Djuri Schiffer <dschiffer@anwb.nl>
2024-11-20 20:34:38 +0100

    add images for gold and silver

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,7 +1,16 @@
 import logo from '../public/images/logo.svg';
+import gold from '../public/images/gold.svg';
+import silver from '../public/images/silver.svg';
 
 export const getImage = (id?: number, format: number = 32) => {
   if (!id) return logo
+
+  if (id === 3575) {
+    return gold
+  }
+  if (id === 3574) {
+    return silver
+  }
 
   return `https://s2.coinmarketcap.com/static/img/coins/${format}x${format}/${id}.png`;
 };
