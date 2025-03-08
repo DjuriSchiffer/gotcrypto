1d5a72ee91954d723588a3fe92cc37819881ebef
Djuri Schiffer <dschiffer@anwb.nl>
2025-03-08 17:07:08 +0100

    removed Fragment

--- src/components/DetailTransactionTable.tsx
+++ src/components/DetailTransactionTable.tsx
@@ -57,36 +57,34 @@ const DetailTransactionTable: React.FC<DetailTransactionTableProps> = ({
                 return yearTransactions.map((transaction: Transaction, index: number) => {
                     const isLastInYear = index === yearTransactions.length - 1;
                     const isYearSeparator = isLastInYear && !isLastYear;
-
+                    
                     return (
-                        <>
-                            <TableRow
-                                key={transaction.id}
-                                type="detail"
-                                item={transaction}
-                                currencies={fetchedCurrencies}
-                                fetchedCurrency={currentFetchedCurrency}
-                                currencyQuote={currencyQuote}
-                                yearCell={index === 0 ? year.toString() : ''}
-                                isYearSeparator={isYearSeparator}
+                        <TableRow
+                            key={transaction.id}
+                            type="detail"
+                            item={transaction}
+                            currencies={fetchedCurrencies}
+                            fetchedCurrency={currentFetchedCurrency}
+                            currencyQuote={currencyQuote}
+                            yearCell={index === 0 ? year.toString() : ''}
+                            isYearSeparator={isYearSeparator}
+                        >
+                            <Button
+                                size="sm"
+                                onClick={() => onEditTransaction(transaction)}
+                                className="mr-2"
+                                color="gray"
                             >
-                                <Button
-                                    size="sm"
-                                    onClick={() => onEditTransaction(transaction)}
-                                    className="mr-2"
-                                    color="gray"
-                                >
-                                    <FaPen color="white" />
-                                </Button>
-                                <Button
-                                    size="sm"
-                                    color="dark"
-                                    onClick={() => onRemoveTransaction(transaction)}
-                                >
-                                    <FaTrashAlt color="white" />
-                                </Button>
-                            </TableRow>
-                        </>
+                                <FaPen color="white" />
+                            </Button>
+                            <Button
+                                size="sm"
+                                color="dark"
+                                onClick={() => onRemoveTransaction(transaction)}
+                            >
+                                <FaTrashAlt color="white" />
+                            </Button>
+                        </TableRow>
                     );
                 });
             })}
