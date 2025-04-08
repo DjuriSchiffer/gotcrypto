5797516b6e0ca1d31e8a1c6ce3b645bdd15ba181
Djuri Schiffer <dschiffer@anwb.nl>
2025-04-08 21:15:03 +0200

    lint services

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,10 +1,12 @@
-import { auth } from '../firebase/firebaseConfig';
+import { FirebaseError } from 'firebase/app';
 import {
-  signInWithPopup,
   GoogleAuthProvider,
   signInAnonymously,
+  signInWithPopup,
   signOut,
 } from 'firebase/auth';
+
+import { auth } from '../firebase/firebaseConfig';
 
 /**
  * Sign in with Google using a popup.
@@ -15,8 +17,14 @@ export const signInWithGoogle = async () => {
     const result = await signInWithPopup(auth, provider);
     // The Auth Context will handle updating the user state
     console.log('Google Sign-In successful:', result.user);
-  } catch (error: any) {
-    console.error('Error during Google Sign-In:', error.message);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      console.error('Error during Google Sign-In:', error.message);
+    } else if (error instanceof Error) {
+      console.error('Error during Google Sign-In:', error.message);
+    } else {
+      console.error('Unknown error during Google Sign-In');
+    }
     // Handle Errors here.
     // You can show error messages to users if needed.
   }
@@ -30,8 +38,14 @@ export const signInAnonymouslyUser = async () => {
     const result = await signInAnonymously(auth);
     // The Auth Context will handle updating the user state
     console.log('Anonymous Sign-In successful:', result.user);
-  } catch (error: any) {
-    console.error('Error during Anonymous Sign-In:', error.message);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      console.error('Error during Anonymous Sign-In:', error.message);
+    } else if (error instanceof Error) {
+      console.error('Error during Anonymous Sign-In:', error.message);
+    } else {
+      console.error('Unknown error during Anonymous Sign-In');
+    }
     // Handle Errors here.
     // You can show error messages to users if needed.
   }
@@ -44,8 +58,14 @@ export const signOutUser = async () => {
   try {
     await signOut(auth);
     console.log('User signed out successfully.');
-  } catch (error: any) {
-    console.error('Error during Sign-Out:', error.message);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      console.error('Error during Sign-Out:', error.message);
+    } else if (error instanceof Error) {
+      console.error('Error during Sign-Out:', error.message);
+    } else {
+      console.error('Unknown error during Sign-Out');
+    }
     // Handle Errors here.
   }
 };
