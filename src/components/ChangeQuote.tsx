01dcb14b6d93c880c8690df30cf39b4eb374f892
Djuri Schiffer <dschiffer@anwb.nl>
2025-04-09 08:22:26 +0200

    lint component/ChangeQuote

--- src/components/ChangeQuote.tsx
+++ src/components/ChangeQuote.tsx
@@ -1,26 +1,28 @@
-import { Button } from "flowbite-react";
-import { FaDollarSign, FaEuroSign } from "react-icons/fa";
-import { CurrencyQuote } from "api";
-import { useQueryClient } from '@tanstack/react-query';
-import { useStorage } from "../hooks/useStorage";
-import { useMemo } from "react";
-import classNames from "classnames";
+import type { CurrencyQuote } from "api";
 
-interface PriceOption {
+import { useQueryClient } from '@tanstack/react-query';
+import classNames from "classnames";
+import { Button } from "flowbite-react";
+import { useMemo } from "react";
+import { FaDollarSign, FaEuroSign } from "react-icons/fa";
+
+import { useStorage } from "../hooks/useStorage";
+
+type PriceOption = {
     quote: keyof CurrencyQuote;
     symbol: React.ReactNode;
 }
 
 export function ChangeQuote({ className }: { className: string }) {
-    const { setCurrencyQuote, currencyQuote } = useStorage();
+    const { currencyQuote, setCurrencyQuote } = useStorage();
     const queryClient = useQueryClient();
 
     const handleQuoteChange = (quote: keyof CurrencyQuote) => {
-        setCurrencyQuote(quote);
-        queryClient.invalidateQueries({ queryKey: ['fetchedCurrencies'] });
+        void setCurrencyQuote(quote);
+        void queryClient.invalidateQueries({ queryKey: ['fetchedCurrencies'] });
     };
 
-    const priceOptions: PriceOption[] = useMemo(() => [
+    const priceOptions: Array<PriceOption> = useMemo(() => [
         {
             quote: 'EUR',
             symbol: <FaEuroSign className="mr-1" />,
@@ -35,14 +37,14 @@ export function ChangeQuote({ className }: { className: string }) {
         <Button.Group className={className}>
             {priceOptions.map((option, index) => (
                 <Button
-                    key={index}
-                    color={currencyQuote === option.quote ? 'dark' : 'gray'}
-                    onClick={() => handleQuoteChange(option.quote)}
                     className={classNames('w-6/12', {
                         '!border-blue-400': currencyQuote === option.quote,
-                        'border-l': index > 0 && currencyQuote === option.quote,
                         '!border-l-[1px]': index > 0 && currencyQuote === option.quote,
+                        'border-l': index > 0 && currencyQuote === option.quote,
                     })}
+                    color={currencyQuote === option.quote ? 'dark' : 'gray'}
+                    key={index}
+                    onClick={() => { handleQuoteChange(option.quote); }}
                 >
                     {option.symbol}
                     {option.quote}
