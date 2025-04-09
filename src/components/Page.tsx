eadf1ecf3fd86a5dc7f71561d598e51bfd24704b
Djuri Schiffer <dschiffer@anwb.nl>
2025-04-09 09:01:15 +0200

    lint component/Page

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,29 +1,32 @@
-import React, { ReactNode, useState } from 'react';
-import SideBar from './SideBar';
-import { FaArrowLeft, FaBars, FaTimes } from 'react-icons/fa';
-import classNames from 'classnames';
+import type { ReactNode } from 'react';
 
-interface PageProps {
+import classNames from 'classnames';
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+import SideBar from './SideBar';
+
+type PageProps = {
   children: ReactNode;
 }
 
-const Page: React.FC<PageProps> = ({ children }) => {
+function Page({ children }: PageProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleSidebar = () => setIsOpen(!isOpen);
+  const toggleSidebar = () => { setIsOpen(!isOpen); };
 
   return (
     <main className="bg-white dark:bg-gray-dark min-h-screen">
       <button
-        onClick={toggleSidebar}
+        aria-label={isOpen ? "Close menu" : "Open menu"}
         className={classNames(
           'fixed top-4 right-4 z-20 p-2',
           'bg-white dark:bg-gray-800',
           'rounded-md shadow-sm',
           'lg:hidden'
         )}
-        aria-label={isOpen ? "Close menu" : "Open menu"}
+        onClick={toggleSidebar}
       >
-        {isOpen ? <FaTimes size={32} color="white" /> : <FaBars size={32} color="white" />}
+        {isOpen ? <FaTimes color="white" size={32} /> : <FaBars color="white" size={32} />}
       </button>
       <div className="p-4">
         <div className="grid gap-4 w-full grid-cols-6">
@@ -32,8 +35,8 @@ const Page: React.FC<PageProps> = ({ children }) => {
             'top-0 bottom-0',
             'transition-all duration-300',
             {
-              'left-0 right-0': isOpen,
               '-left-full right-full': !isOpen,
+              'left-0 right-0': isOpen,
             },
             'lg:static lg:left-auto lg:right-auto lg:z-auto'
           )}>
